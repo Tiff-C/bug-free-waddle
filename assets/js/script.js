d98520b36aafb3865237b4cfab67dee746f85064
Tiff-C <deguntiffany@gmail.com>
2024-01-14 14:51:00 +0000

    Use ternary operators to display component title prop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 
-const domNode = document.getElementById('app');
-const root = ReactDOM.createRoot(domNode);
-root.render(<h1>Test</h1>);
+const app = document.getElementById('app');
+
+function Header({ title }) {
+  console.log(title); 
+  return (<h1>{title ? title : 'Default title'}</h1>);
+}
+
+function HomePage() {
+  return (
+    <div>
+      {/* Nesting the Header component */}
+      <Header title="React"/>
+      <Header title="A new title" />
+      <Header/>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(app);
+root.render(<HomePage/>);
